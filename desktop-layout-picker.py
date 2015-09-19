a46ec3f9ea3457ea611d1876590375e6b4f7e369
dude56987 <dude56987@users.noreply.github.com>
2015-09-19 13:46:57 -0400

    Fixed a bug, Code efficiency improvements
    - Fixed a bug that closed the panel and did not open it back up
      - Program checks for xfce-panel to be closed before starting it back
    - Made the import statements more specific
    - Removed sleep code

--- desktop-layout-picker.py
+++ desktop-layout-picker.py
@@ -16,9 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ########################################################################
-import os
+from os import system
+from os import popen
+from os import listdir
 from dialog import Dialog
-from time import sleep
 ########################################################################
 # This program changes the current users desktop layout for xfce4
 ########################################################################
@@ -32,7 +33,7 @@ config=''
 # Build the user interface 
 ########################################################################
 # grab a list of available desktop settings for the user to pick from
-for item in os.listdir("/opt/hackbox/preconfiguredSettings/userSettings/"):
+for item in listdir("/opt/hackbox/preconfiguredSettings/userSettings/"):
 	# ignore core settings,hidden files, and any files that are not folders
 	if (item != "CORE") and (("." in item) != True):
 		if "default" in item:
@@ -50,21 +51,21 @@ if len(choices)>1:
 	desktopLine=userChoice
 ########################################################################
 # stop xfce4-panel and kill xfconfd which stores settings in ram
-os.system('xfce4-panel --quit')
-os.system('pkill xfconfd')
+system('xfce4-panel --quit')
+system('pkill xfconfd')
 # remove users old config files
 print('rm -rv ~/.config/xfce4/panel/')
-os.system('rm -rv ~/.config/xfce4/panel/')
+system('rm -rv ~/.config/xfce4/panel/')
 print('rm -v ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml')
-os.system('rm -v ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml')
+system('rm -v ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml')
 print('rm -v ~/.config/xfce4/xfce4-panel.xml')
-os.system('rm -v ~/.config/xfce4/xfce4-panel.xml')
+system('rm -v ~/.config/xfce4/xfce4-panel.xml')
 # install user picked settings package into the current users home directory
 if len(desktopLine) > 1:
 	print("cp -rvf "+desktopLine+"/.* ~/")
-	os.system("cp -rvf "+desktopLine+"/.* ~/")
-# sleep 1 second to allow system to catch up ending xfce panel before starting it back
-sleep(1)
-# reset the xfce4 panel to show new settings
-print('nohup xfce4-panel > /dev/null &')
-os.system('nohup xfce4-panel > /dev/null &')
+	system("cp -rvf "+desktopLine+"/.* ~/")
+# if xfce4-panel has been closed successfully relaunch it
+while (('xfce4-panel' in popen('ps -e').read()) != True):
+	# start the xfce4 panel to show new settings
+	print('nohup xfce4-panel > /dev/null &')
+	system('nohup xfce4-panel > /dev/null &')
